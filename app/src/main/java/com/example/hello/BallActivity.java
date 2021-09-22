4db8678d5ea280a1f36f746aab134e53779dccfd
kaka473 <1095503131@qq.com>
2021-09-22 21:04:09 +0800

    Initial commit

---
+++ app/src/main/java/com/example/hello/BallActivity.java
@@ -0,0 +1,52 @@
+package com.example.hello;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class BallActivity extends AppCompatActivity{
+
+    int score1=0;
+    int score2=0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ball);
+
+    }
+    public void click(View btn){
+        if(btn.getId()==R.id.btn3){
+            score1+=3;
+        }else if(btn.getId()==R.id.btn2){
+            score1+=2;
+        }else if(btn.getId()==R.id.btn1){
+            score1++;
+        }else
+        {
+            score1=0;
+            score2=0;
+        }
+        ShowScore();
+    }
+
+    private void ShowScore() {
+        TextView show1 = findViewById(R.id.score1);
+        show1.setText(String.valueOf(score1));
+        TextView show2 = findViewById(R.id.score2);
+        show2.setText(String.valueOf(score2));
+    }
+
+    public void clickb(View btn){
+        if(btn.getId()==R.id.btnb3){
+            score2+=3;
+        }else if(btn.getId()==R.id.btnb2){
+            score2+=2;
+        }else if(btn.getId()==R.id.btnb1){
+            score2++;
+        }
+        ShowScore();
+    }
+}
